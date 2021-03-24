a22d3699b0239adcdf517b31a1cc1720506a82e8
Travis <tahalarewich@live.ca>
2021-03-24 15:49:26 -0600

    started implementation of site login/Bnet connect

--- app.py
+++ app.py
@@ -1,7 +1,7 @@
 from flask import Flask, redirect, render_template, request, session, url_for
 from flask_session import Session
+from werkzeug.security import check_password_hash, generate_password_hash
 from authlib.integrations.flask_client import OAuth
-
 import sqlite3
 
 # config app
@@ -37,13 +37,63 @@ def SQL(query):
 @app.route("/", methods=["GET", "POST"])
 #@login_required
 def index():
-    # resp = get.("")
-    # user_info = resp.json()
     battle_tag = dict(session).get("battletag", None)  
-    return f"hello {battle_tag}"
+    return f"hello {battle_tag} user {session.user_id}"
+
+@app.route("/login", methods=["GET", "POST"])
+def login():
+    session.clear()
+    if request.method == "POST":
+        username = request.form.get("username")
+        password = request.form.get("password")
+        # Checks if username field was left blank
+        if not username:
+            pass
+            # return error("Must enter username")
+        elif not password:
+            pass
+            # return error("Must enter password")
+        # Checks if username is in the Database
+        sql_query = SQL(("SELECT * FROM users WHERE username = ?", username))
+        if not len(sql_query) == 1 or not check_password_hash(sql_query[0]["hash"], password):
+            # return error("No registered account with that username and password")
+            pass
+        session["user_id"] = sql_query[0]["id"]
+        return redirect("/")
+    else:
+        return render_template("login.html")
+
+
+@app.route("/logout")
+def logout():
+    session.clear()
+    return render_template("login.html")
+
+@app.route("/register", methods=["GET", "POST"])
+def register():
+    if request.method == "POST":
+        # grab user input from form
+        username = request.form.get("username")
+        password = request.form.get("password")
+        confirm = request.form.get("confirm")
+        # verify user input
+        query_result = SQL(("SELECT COUNT(id) FROM users WHERE username = ?", username))
+        if query_result[0]["COUNT(id)"] == 1 or username == "":
+            pass
+            #return error(Username already taken or field was left blank)
+        elif password == "" or not password == confirm:
+            pass
+            #return error(Passwords don't match or field was left blank)
+        else:
+            # hash password to store in DB
+            PWhash = generate_password_hash(password, method='pbkdf2:sha256')
+            SQL(("INSERT INTO users (username, hash) VALUES (?, ?)", username, PWhash))
+        return render_template("login.html")
+    else:
+        return render_template("register.html") 
 
 @app.route("/battlenet_login")
-def login():
+def battlenet_login():
     hearthstone = oauth.create_client("hearthstone")
     redirect_uri = url_for("authorize", _external=True)
     return hearthstone.authorize_redirect(redirect_uri)
@@ -55,6 +105,6 @@ def authorize():
     resp = hearthstone.get("oauth/userinfo")
     resp.raise_for_status()
     user = resp.json()
-    session["battletag"] = user["battletag"]
-    # do something with the token and profile
+    # store 
+    SQL(("UPDATE users SET token = ?, battle_tag = ? WHERE id = ?", token, user["battle_tag"], session.user_id))
     return redirect("/")
